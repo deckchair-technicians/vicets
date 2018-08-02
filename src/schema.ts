f045fef7db260e9f90996d84a2aaa23b8c7a7108
Matt Savage <matthew.savage@gmail.com>
2018-08-02 22:15:47 +0100

    Minor: reformat

--- src/schema.ts
+++ src/schema.ts
@@ -4,70 +4,70 @@ import {RecordSchema} from "./records";
 export type Path = any[];
 
 export class Problem {
-    constructor(readonly path: Path, readonly message: string) {
-    }
+  constructor(readonly path: Path, readonly message: string) {
+  }
 
-    prefixPath(p: Path): Problem {
-        return new Problem([...p, ...this.path], this.message);
-    }
+  prefixPath(p: Path): Problem {
+    return new Problem([...p, ...this.path], this.message);
+  }
 
-    toString(): string {
-        return `[${this.path.join(' ').trimRight()}] : ${this.message}`
-    }
+  toString(): string {
+    return `[${this.path.join(' ').trimRight()}] : ${this.message}`
+  }
 }
 
 export class Problems {
-    constructor(readonly problems: Problem[]) {
-    }
+  constructor(readonly problems: Problem[]) {
+  }
 
-    prefixPath(p: Path): Problems {
-        return new Problems(this.problems.map(e => e.prefixPath(p)));
+  prefixPath(p: Path): Problems {
+    return new Problems(this.problems.map(e => e.prefixPath(p)));
 
-    }
+  }
 
-    merge(...ps: Problems[]): Problems {
-        return ps.reduce((acc: Problems, next: Problems) => acc.append(...next.problems), this);
+  merge(...ps: Problems[]): Problems {
+    return ps.reduce((acc: Problems, next: Problems) => acc.append(...next.problems), this);
 
-    }
+  }
 
-    append(...ps: Problem[]): Problems {
-        return new Problems([...this.problems, ...ps]);
+  append(...ps: Problem[]): Problems {
+    return new Problems([...this.problems, ...ps]);
 
-    }
+  }
 
-    toString(): string {
-        return this.problems.map(e => e.toString()).join("\r\n")
-    }
+  toString(): string {
+    return this.problems.map(e => e.toString()).join("\r\n")
+  }
 }
 
 export function isError(x: Problems | any): boolean {
-    return x != null && x instanceof Problems;
+  return x != null && x instanceof Problems;
 }
 
 export function isSuccess(x: Problems | any): boolean {
-    return !isError(x);
+  return !isError(x);
 }
 
 export function problem(message: string, path: Path = []) {
-    return new Problem(path, message);
+  return new Problem(path, message);
 }
 
 export function problems(problem: Problem, ...more: Problem[]) {
-    return new Problems([...[problem], ...more]);
+  return new Problems([...[problem], ...more]);
 }
 
 export function failure(message: string, path: Path = []): Problems {
-    return problems(problem(message, path));
+  return problems(problem(message, path));
 }
 
 export type ValidationResult = Problems | any;
 
 export interface Schema<IN, OUT> {
-    conform(this: this, value: IN): Problems | OUT
+  conform(this: this, value: IN): Problems | OUT
 
-    and<NEWOUT>(this: this, s: Schema<OUT, NEWOUT>): Schema<IN, NEWOUT>
+  and<NEWOUT>(this: this, s: Schema<OUT, NEWOUT>): Schema<IN, NEWOUT>
 
-    or<NEWIN extends IN, NEWOUT>(this: this, s: Schema<IN, NEWOUT>): Schema<IN, OUT | NEWOUT>
+  or<NEWIN extends IN, NEWOUT>(this: this, s: Schema<IN, NEWOUT>): Schema<IN, OUT | NEWOUT>
 
     __(this: this): OUT
 }
@@ -76,67 +76,68 @@ export function __<IN, OUT>(s: Schema<IN, OUT>): OUT {
     return s as any as OUT;
 }
 
-export function record<T>(constructor: {new(...args:any[]):T}): RecordSchema<T> {
-    return new RecordSchema(constructor);
+export function record<T>(constructor: { new(...args: any[]): T }): Schema<any, T> {
+  return new RecordSchema(constructor);
 }
 
-export function eq<T>(y: T): Schema<any,T> {
-    return predicate<T>(
-        (x) => x === y,
-        (x) => `expected ${y} but got ${x}`);
+export function eq<T>(y: T): Schema<any, T> {
+  return predicate<T>(
+    (x) => x === y,
+    (x) => `expected ${y} but got ${x}`);
+}
 }
 
 export function schema<IN, OUT>(conform: (value: IN) => Problems | OUT): Schema<IN, OUT> {
-    return new DelegatingSchema<IN, OUT>(conform);
+  return new DelegatingSchema<IN, OUT>(conform);
 }
 
 function _buildPredicateMessageFunction(message: ((value: any) => string) | string | undefined, predicate: (x: any) => boolean): (value: any) => string {
-    switch (typeof  message) {
-        case 'string':
-            return (value: any) => message as string;
-        case 'function':
-            return message as (value: any) => string;
-        case 'undefined':
-            return (value: any) => predicate.toString();
-        default:
-            throw new Error(`Not a valid message ${message}`);
-    }
+  switch (typeof  message) {
+    case 'string':
+      return (value: any) => message as string;
+    case 'function':
+      return message as (value: any) => string;
+    case 'undefined':
+      return (value: any) => predicate.toString();
+    default:
+      throw new Error(`Not a valid message ${message}`);
+  }
 }
 
 
 export function predicate<T>(predicate: (value: T) => boolean,
                              failureMessage?: ((value: any) => string) | string): Schema<T, T> {
-    let messageFn = _buildPredicateMessageFunction(failureMessage, predicate);
-    return schema(
-        (x) => predicate(x) === true ? x : failure(messageFn(x)))
+  let messageFn = _buildPredicateMessageFunction(failureMessage, predicate);
+  return schema(
+    (x) => predicate(x) === true ? x : failure(messageFn(x)))
 }
 
 
 export type Schemaish = { conform(x: any): any } | Function | object;
 
 export function schematize<IN, OUT>(x: Schemaish): Schema<IN, OUT> {
-    let t = typeof x;
-    if (t === "function")
-        return predicate(x as (x: any) => boolean);
+  let t = typeof x;
+  if (t === "function")
+    return predicate(x as (x: any) => boolean);
 
-    if (t === "object") {
-        if ('conform' in x && typeof x.conform === "function")
-            return x as Schema<IN, OUT>;
-        else
-            return new ObjectSchema(x) as any as Schema<IN, OUT>;
-    }
-    throw Error(`Cannot build schema from ${x}`);
+  if (t === "object") {
+    if ('conform' in x && typeof x.conform === "function")
+      return x as Schema<IN, OUT>;
+    else
+      return new ObjectSchema(x) as any as Schema<IN, OUT>;
+  }
+  throw Error(`Cannot build schema from ${x}`);
 }
 
-export function object<T extends object>(object: Object): ObjectSchema {
-    return new ObjectSchema(object);
+export function object<T extends object>(object: Object): Schema<object, object> {
+  return new ObjectSchema(object);
 }
 
 export function assertSchema(s: Schemaish, value: any): any {
-    const result = schematize(s).conform(value);
-    if (isSuccess(result)) return result;
+  const result = schematize(s).conform(value);
+  if (isSuccess(result)) return result;
 
-    let e = new Error(`${result}`);
-    e['problems'] = result;
-    throw e;
+  let e = new Error(`${result}`);
+  e['problems'] = result;
+  throw e;
 }
--- test/records.test.ts
+++ test/records.test.ts
@@ -4,8 +4,8 @@ import {__, eq, record, schema} from "../src/schema";
 
 @data
 class Test {
-    equals1: number = __(eq(1));
-    coercedValue: number = __(schema((_) => 123));
+  equals1: number = __(eq(1));
+  coercedValue: number = __(schema((_) => 123));
 }
 
 // Schema and class are defined in the same place
@@ -21,46 +21,49 @@ class Test {
 
 @data
 class Parent {
-    parentField: number = __(eq(1));
+  parentField: number = __(eq(1));
 }
 
 @data
 class Child extends Parent {
-    childField: number = __(eq(1));
+  childField: number = __(eq(1));
 }
 
 @data
 class FieldsInConstructor {
-    constructor(public field: number = __(eq(1))) {
-    }
+  constructor(public field: number = __(eq(1))) {
+  }
 }
 
 @data
 class ChildWithFieldsInConstructor extends FieldsInConstructor {
-    constructor(public childField: number = __(eq(1)),
-                field: number) {
+  constructor(public childField: number = __(eq(1)),
+              field: number) {
 
-        super(field);
-    }
+    super(field);
+  }
 }
 
 @data
 class Nested {
-    constructor(public a: string = __(eq("valid"))) {
-    }
+  constructor(public a: string = __(eq("valid"))) {
+  }
 }
 
 @data
 class HasNested {
-    constructor(public nested: Nested = __(record(Nested))) {
-    }
+  constructor(public nested: Nested = __(record(Nested))) {
+  }
 }
 
 @data
 class Union {
-    constructor(
-        public union: string | number
-            = eq("valid").or(eq(1)).__(),
+  constructor(public union: string | number
+                = eq("valid").or(eq(1)).__(),
+              public single: string
+                = eq("valid").or(eq("also valid")).__()) {
+  }
+}
 
         public single: string
             = eq("valid").or(eq("also valid")).__()) {
@@ -68,72 +71,72 @@ class Union {
 }
 
 describe('data', () => {
-    it('Should allow fields to be defined in the constructor', () => {
-        expect(new FieldsInConstructor(1)).deep.equals({field: 1});
-        expect(() => new FieldsInConstructor(0)).to.throw(Error);
-    });
+  it('Should allow fields to be defined in the constructor', () => {
+    expect(new FieldsInConstructor(1)).deep.equals({field: 1});
+    expect(() => new FieldsInConstructor(0)).to.throw(Error);
+  });
 
-    it('Should allow fields to be defined in the constructor, and work with inheritance', () => {
-        expect(build(ChildWithFieldsInConstructor, {childField: 1, field: 1}),
-            "valid object")
-            .deep.equals({field: 1, childField: 1});
-        expect(() => build(ChildWithFieldsInConstructor, {childField: 1, field: 0}),
-            "invalid parent __").to.throw(Error);
-        expect(() => build(ChildWithFieldsInConstructor, {childField: 0, field: 1}),
-            "invalid child __").to.throw(Error);
+  it('Should allow fields to be defined in the constructor, and work with inheritance', () => {
+    expect(build(ChildWithFieldsInConstructor, {childField: 1, field: 1}),
+      "valid object")
+      .deep.equals({field: 1, childField: 1});
+    expect(() => build(ChildWithFieldsInConstructor, {childField: 1, field: 0}),
+      "invalid parent __").to.throw(Error);
+    expect(() => build(ChildWithFieldsInConstructor, {childField: 0, field: 1}),
+      "invalid child __").to.throw(Error);
 
-        expect(new ChildWithFieldsInConstructor(1, 1)).deep.equals({field: 1, childField: 1});
-        expect(() => new ChildWithFieldsInConstructor(1, 0)).to.throw(Error);
-        expect(() => new ChildWithFieldsInConstructor(0, 1)).to.throw(Error);
-    });
+    expect(new ChildWithFieldsInConstructor(1, 1)).deep.equals({field: 1, childField: 1});
+    expect(() => new ChildWithFieldsInConstructor(1, 0)).to.throw(Error);
+    expect(() => new ChildWithFieldsInConstructor(0, 1)).to.throw(Error);
+  });
 
-    it('Should apply schema when constructing', () => {
-        expect(build(Test,
-            {
-                equals1: 1,
-                coercedValue: 456
-            })).deep.equals(
-            {
-                equals1: 1,
-                coercedValue: 123
-            });
+  it('Should apply schema when constructing', () => {
+    expect(build(Test,
+      {
+        equals1: 1,
+        coercedValue: 456
+      })).deep.equals(
+      {
+        equals1: 1,
+        coercedValue: 123
+      });
 
-    });
+  });
 
-    it('Should cope with inheritance', () => {
-        expect(build(Parent, {parentField: 1})).deep.equals({parentField: 1});
-        expect(() => build(Parent, {parentField: 0})).to.throw(Error);
+  it('Should cope with inheritance', () => {
+    expect(build(Parent, {parentField: 1})).deep.equals({parentField: 1});
+    expect(() => build(Parent, {parentField: 0})).to.throw(Error);
 
-        expect(build(Child, {parentField: 1, childField: 1})).deep.equals({parentField: 1, childField: 1});
-        expect(() => build(Child, {
-                parentField: 0,
-                childField: 0
-            }
-        )).to.throw(Error);
-    });
-    it('Should cope with inheritance where parent is also data', () => {
-        expect(build(Parent, {parentField: 1})).deep.equals({parentField: 1});
-        expect(() => build(Parent, {parentField: 0})).to.throw(Error);
+    expect(build(Child, {parentField: 1, childField: 1})).deep.equals({parentField: 1, childField: 1});
+    expect(() => build(Child, {
+        parentField: 0,
+        childField: 0
+      }
+    )).to.throw(Error);
+  });
+  it('Should cope with inheritance where parent is also data', () => {
+    expect(build(Parent, {parentField: 1})).deep.equals({parentField: 1});
+    expect(() => build(Parent, {parentField: 0})).to.throw(Error);
 
-        expect(build(Child, {parentField: 1, childField: 1})).deep.equals({parentField: 1, childField: 1});
-        expect(() => build(Child, {parentField: 0, childField: 0})).to.throw(Error);
-    });
+    expect(build(Child, {parentField: 1, childField: 1})).deep.equals({parentField: 1, childField: 1});
+    expect(() => build(Child, {parentField: 0, childField: 0})).to.throw(Error);
+  });
 
-    it('Should cope with nesting', () => {
-        expect(new HasNested(new Nested("valid"))).deep.equals({nested: {a: "valid"}});
-        expect(() => new HasNested()).to.throw(Error);
+  it('Should cope with nesting', () => {
+    expect(new HasNested(new Nested("valid"))).deep.equals({nested: {a: "valid"}});
+    expect(() => new HasNested()).to.throw(Error);
 
-        expect(build(HasNested, {nested: {a: "valid"}})).deep.equals({nested: {a: "valid"}});
-        expect(() => build(HasNested, {nested: {a: "not valid"}})).to.throw(Error);
-    });
-    it('Should support union types', () => {
-        expect(new Union(1, "also valid")).deep.equals({union:1, single:"also valid"});
-        expect(() => new Union(2, "valid")).to.throw(Error);
-        expect(() => new Union(1, "not valid")).to.throw(Error);
+    expect(build(HasNested, {nested: {a: "valid"}})).deep.equals({nested: {a: "valid"}});
+    expect(() => build(HasNested, {nested: {a: "not valid"}})).to.throw(Error);
+  });
+  it('Should support union types', () => {
+    expect(new Union(1, "also valid")).deep.equals({union: 1, single: "also valid"});
+    expect(() => new Union(2, "valid")).to.throw(Error);
+    expect(() => new Union(1, "not valid")).to.throw(Error);
 
-        expect(build(Union, {union: 1, single: "also valid"})).deep.equals({union: 1, single: "also valid"});
-        expect(build(Union, {union: "valid", single: "valid"})).deep.equals({union: "valid", single: "valid"});
-        expect(() => build(Union, {union: "not valid", single: "valid"})).to.throw(Error);
-        expect(() => build(Union, {union: "valid", single: "not valid"})).to.throw(Error);
-    });
+    expect(build(Union, {union: 1, single: "also valid"})).deep.equals({union: 1, single: "also valid"});
+    expect(build(Union, {union: "valid", single: "valid"})).deep.equals({union: "valid", single: "valid"});
+    expect(() => build(Union, {union: "not valid", single: "valid"})).to.throw(Error);
+    expect(() => build(Union, {union: "valid", single: "not valid"})).to.throw(Error);
+  });
 });
