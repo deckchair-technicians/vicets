06d4d4f1154540b6919e8bbe8a5d0bcd2e54ac67
Matt Savage <matthew.savage@gmail.com>
2018-08-17 13:22:38 +0100

    Cleaning up data tests

---
+++ test/data/definition_in_constructor.test.ts
@@ -0,0 +1,54 @@
+import {__, build, data} from "../../index";
+import {eq} from "../../src/schemas";
+import {expect} from "chai";
+
+describe('Defining fields in constructor', () => {
+  @data
+  class FieldsInConstructor {
+    constructor(public field: number = __(eq(1))) {
+    }
+  }
+
+  it('should allow fields to be defined in the constructor', () => {
+    expect(new FieldsInConstructor(1)).deep.equals({field: 1});
+    expect(() => new FieldsInConstructor(0)).to.throw(/expected '1'/);
+  });
+
+  it('still works with build', () => {
+    expect(build(FieldsInConstructor, {field:1})).deep.equals({field: 1});
+    expect(() => build(FieldsInConstructor, {field:2})).to.throw(/expected '1'/);
+  });
+
+  it('does not keep constructor defaults if not overridden in constructor arguments', () => {
+    // Without removing field values that are schemas, this would be complaining that field
+    // is an EqualsSchema
+    expect(() => new FieldsInConstructor()).to.throw(/expected '1' but got undefined/);
+  });
+
+  @data
+  class ChildWithFieldsInConstructor extends FieldsInConstructor {
+    constructor(public childField: number = __(eq(2)),
+                field: number) {
+
+      super(field);
+    }
+  }
+
+  describe('Inheritance', () => {
+    it('works with valid values', ()=>{
+      expect(new ChildWithFieldsInConstructor( 2, 1))
+        .deep.equals({field: 1, childField: 2});
+    });
+    it('works with invalid child value', ()=>{
+      expect(()=>new ChildWithFieldsInConstructor(0, 1)).to.throw(Error);
+    });
+    it('works with invalid parent value', ()=>{
+      expect(()=>new ChildWithFieldsInConstructor(2, 2)).to.throw(Error);
+    });});
+
+  it('still works with build', () => {
+    expect(build(ChildWithFieldsInConstructor, {field:1, childField:2})).deep.equals({field:1, childField:2});
+    expect(() => build(ChildWithFieldsInConstructor, {field:2, childField:2})).to.throw(/expected '1'/);
+    expect(() => build(ChildWithFieldsInConstructor, {field:1, childField:1})).to.throw(/expected '2'/);
+  });
+});
---
+++ test/data/definition_in_field_declaration.test.ts
@@ -0,0 +1,90 @@
+import {expect} from "chai";
+import {__, build, data, eq, isdata} from "../../index";
+
+
+describe('@data decorator', () => {
+  describe('Inheritance', () => {
+    @data
+    class Parent {
+      parentField: string = __(eq("valid"));
+    }
+
+    @data
+    class Child extends Parent {
+      childField: string = __(eq("valid"));
+    }
+
+    it('does not affect parent', () => {
+      expect(build(Parent, {parentField: "valid"})).deep.equals({parentField: "valid"});
+      expect(() => build(Parent, {parentField: "some bad value"})).to.throw(/some bad value/);
+    });
+    it('returns errors from parent schema', () => {
+      expect(() => build(Child, {childField: "valid", parentField: "some bad value"})).to.throw(/some bad value/);
+    });
+    it('returns errors from child schema', () => {
+      expect(() => build(Child, {childField: "some bad value", parentField: "valid"})).to.throw(/some bad value/);
+    });
+  });
+  describe('nesting', () => {
+    @data
+    class Nested {
+      a: string = __(eq("valid"));
+    }
+
+    @data
+    class HasNested {
+      nested: Nested = __(isdata(Nested));
+    }
+
+    it('works for valid input', () => {
+      expect(build(HasNested, {nested: {a: "valid"}}))
+        .deep.equals({nested: {a: "valid"}});
+    });
+    it('works for invalid input', () => {
+      expect(() => build(HasNested, {nested: {a: "not a valid value"}}))
+        .to.throw(/not a valid value/);
+    });
+  });
+
+  describe('Union type fields', () => {
+    it('can use .or() to specify schema', () => {
+      @data
+      class OrSchemaWithUnionType {
+        union: string | number = eq("valid").or(eq(1)).__();
+      }
+
+      expect(build(OrSchemaWithUnionType, {union: 1})).deep.equals({union: 1});
+      expect(build(OrSchemaWithUnionType, {union: "valid"})).deep.equals({union: "valid"});
+      expect(() => build(OrSchemaWithUnionType, {union: "some bad value"})).to.throw(/some bad value/);
+    });
+    it('.or() works as expected when all schemas produce the same type', () => {
+      // We're just checking that this compiles
+      @data
+      class OrSchemaWithSingleType {
+        // Note- field type is string
+        union: string = eq("valid").or(eq("also valid")).__();
+      }
+    });
+  });
+});
+
+xit('should support custom constructors(?)', () => {
+});
+xit('should complain if subclasses redefine fields', () => {
+});
+xit('should provide indication of field requirements if a field is missing', () => {
+});
+xit('should support optional fields', () => {
+});
+xit('missing fields', () => {
+});
+xit('additional fields', () => {
+});
+xit('nice error message for fields missing schema', () => {
+});
+xit('should support adding additional methods', () => {
+});
+xit('supports recursion', () => {
+});
+
+
