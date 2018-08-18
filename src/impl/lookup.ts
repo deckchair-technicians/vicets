517ff047b4798dc02db9d7f1b53c39170d424167
Matt Savage <matthew.savage@gmail.com>
2018-08-18 21:32:31 +0100

    Added lookup()

---
+++ src/impl/lookup.ts
@@ -0,0 +1,19 @@
+import {BaseSchema} from "./index";
+import {failure, Problems} from "../problems";
+import {typeDescription} from "./util";
+
+export class LookupSchema<T extends object,V> extends BaseSchema<any, T[keyof T]> {
+  constructor(private readonly e: T) {
+    super();
+  }
+
+  conform(value: any): Problems | T[keyof T] {
+    if(typeof value !== 'string')
+      return failure(`expected a string but got ${typeDescription(value)}`);
+
+    if(value in this.e)
+      return this.e[value];
+
+    return failure(`expected one of [${Object.keys(this.e).map((k) => JSON.stringify(k)).join(', ')}]`)
+  }
+}
---
+++ test/schemas/lookup.test.ts
@@ -0,0 +1,59 @@
+import {expect} from 'chai';
+import {lookup} from "../../src/schemas";
+import {Schema} from "../../src/schema";
+import {Problems} from "../../src/problems";
+
+describe('lookup', () => {
+  describe('Basics', () => {
+    const s: Schema<any, string> = lookup({
+      a: "a value",
+      b: "b value"
+    });
+
+    it('looks values up in the provided object', () => {
+      expect(s.conform('a')).to.equal('a value');
+      expect(s.conform('b')).to.equal('b value');
+    });
+    it("returns problems if the key doesn't exist", () => {
+      expect(s.conform('c')).deep.equals({
+        problems: [
+          {
+            message: 'expected one of ["a", "b"]',
+            path: []
+          }
+        ]
+      });
+    });
+    it("returns problems if value isn't a string", () => {
+      expect(s.conform({})).deep.equals({
+        problems: [
+          {
+            message: 'expected a string but got object',
+            path: []
+          }
+        ]
+      });
+    });
+  });
+  describe('Generic signature', () => {
+    it('figures out type of output based on shape of class', () => {
+      // Mostly just demonstrating that this compiles
+      class A {
+        constructor(public a: string,
+                    public b: number) {
+        }
+      }
+
+      const s: Schema<any, A[keyof A]> = lookup(new A('a value', 123));
+      const value: Problems | string | number = s.conform('a');
+      expect(value).to.equal('a value')
+    });
+
+    it('figures out type of output based on shape of literal', () => {
+      // Mostly just  demonstrating that this compiles
+      const s: Schema<any, string | number> = lookup({a: "a value", b: 123});
+      const value: Problems | string | number = s.conform('a');
+      expect(value).to.equal('a value')
+    });
+  });
+});
