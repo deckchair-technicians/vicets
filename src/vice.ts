30d1df9925a5f92a7cbda2d9a9f22b4fa891d17b
Matt Savage <matthew.savage@gmail.com>
2019-05-23 17:29:45 +0100

    Export like from vice.ts

--- src/vice.ts
+++ src/vice.ts
@@ -6,6 +6,7 @@ export * from "./schematize";
 export * from "./unexpected_items"
 export * from "./hasschema";
 export * from "./helpers";
+export * from "./mocha";
 export * from "./impl";
 export {StrictPattern, StrictPatternItem, Pattern, PatternItem} from "./impl/associative/associative";
 export {patternItemToSchema} from "./impl/associative/obj";
